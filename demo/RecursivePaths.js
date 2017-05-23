6d69cbf7759eb24427409a828a6dda36dea7621a
likun7981 <likun7981@gmail.com>
2017-05-23 23:24:36 +0800

    change package auto-breadcrumb

--- demo/RecursivePaths.js
+++ demo/RecursivePaths.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import BreadcrumbConfig from '../src';
+import BreadcrumbConfig from 'auto-breadcrumb';
 
 const Breadcrumbs = BreadcrumbConfig({
   staticRoutesMap: {
